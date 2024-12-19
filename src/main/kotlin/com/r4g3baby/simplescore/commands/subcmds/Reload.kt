ff6326a236c66c1fe3752d1c5aa766f2b83797c4
ramiro33 <107439581+ramiro33@users.noreply.github.com>
2024-12-18 23:27:37 -0300

    Update Reload.kt

--- src/main/kotlin/com/r4g3baby/simplescore/commands/subcmds/Reload.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/commands/subcmds/Reload.kt
@@ -1,14 +1,13 @@
-package com.r4g3baby.simplescore.commands.subcmds
+package com.r4g3baby.hyperboard.commands.subcmds
 
-import com.r4g3baby.simplescore.SimpleScore
-import com.r4g3baby.simplescore.SimpleScore.Api.i18n
-import com.r4g3baby.simplescore.commands.SubCmd
+import com.r4g3baby.hyperboard.HyperBoard
+import com.r4g3baby.hyperboard.commands.SubCmd
 import org.bukkit.command.CommandSender
 
 class Reload : SubCmd("reload") {
     override fun run(sender: CommandSender, args: Array<out String>) {
-        sender.sendMessage(i18n.t("cmd.reload.start"))
-        SimpleScore.reload()
-        sender.sendMessage(i18n.t("cmd.reload.finished"))
+        sender.sendMessage("§aRecargando la configuración de HyperBoard...")  // Mensaje de inicio
+        HyperBoard.reload()  // Llamada al método de recarga de HyperBoard
+        sender.sendMessage("§aRecarga completada.")  // Mensaje de finalización
     }
 }
